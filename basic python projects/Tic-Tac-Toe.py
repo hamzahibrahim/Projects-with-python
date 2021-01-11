a656b27af72c82c11c78ce09f16565c673229f8f
Hamzah Jodeh <hamzahibrahimjodeh5@gmail.com>
2021-01-12 00:34:37 +0200

    Modified Tic-Tac-Toe.py In basic python projects

--- basic python projects/Tic-Tac-Toe.py
+++ basic python projects/Tic-Tac-Toe.py
@@ -1,185 +1,113 @@
 import random
 
-board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']       
-       
-def Draw_Board():    
-    print(" {} | {} | {} ".format(board[0],board[1],board[2]))    
-    print("___|___|___")    
-    print(" {} | {} | {} ".format(board[3], board[4], board[5]))
-    print("___|___|___")    
-    print(" {} | {} | {} ".format(board[6],board[7],board[8]))    
-    print("   |   |   ")      
+board = ["  " for i in range(9)]
 
-def is_victory(l,b=board):
-    return ((b[0] == l and b[1] == l and b[2] == l) or
-            (b[3] == l and b[4] == l and b[5] == l) or
-            (b[6] == l and b[7] == l and b[8] == l) or
-            (b[2] == l and b[3] == l and b[6] == l) or
-            (b[1] == l and b[4] == l and b[7] == l) or
-            (b[2] == l and b[5] == l and b[8] == l) or
-            (b[0] == l and b[4] == l and b[8] == l) or
-            (b[2] == l and b[4] == l and b[6] == l))
+def print_board():
+    row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
+    row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
+    row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
 
-def is_tie(b=board):
-    return ((b[0] != ' ' and b[1] != ' ' and b[2] != ' ' and b[3] != ' ' and b[4] != ' ' and b[5] != ' ' and b[6] != ' ' and b[7] != ' ' and b[8] != ' ' )\
-            and(not is_victory('X') and not is_victory('O')))
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()    
+    
+    
 
-def space_is_free(pos):
-    return board[pos] == ' '
 
-def Player_move(): 
-    go = True
-    while go: 
-        user_choice = input('Please enter a number between [1-9]: ')
-        try:
-            user_choice = int(user_choice)
-            if user_choice > 0 and user_choice < 10:
-                user_choice -= 1
-                if space_is_free(user_choice):
-                    board[user_choice] = 'X'
-                    go = False
-                else:
-                    print('This space is Taken!')    
-            else:
-                print('Did you type between [1-9]?')    
-
-        except :
-            print('Did you type a number Please type a number: ')
-
-def computer_move():
-    go = True
-    while go: 
-        computer_choice = random.randint(1,9)
-        computer_choice -= 1
-        if is_tie():
-            go = False
-            break  
-        elif is_victory('X') or is_victory('O'):
-            go = False
-            break
-        elif space_is_free(computer_choice):   
-            board[computer_choice] = 'O'
-            go = False
-            break
-
-def one_player_mode():
-    print('You have picked the one player mode, you are X computer is O -_-')
-    i = input('Do you want to start y for yes anything else computer will start: ')
-    if i.lower() == 'y':
-        while True:
-            Draw_Board()
-            Player_move()
-            if is_victory('X')and is_tie() :
-                Draw_Board()
-                print("\nYou win Well Done!")
-                break
-            elif is_victory('O') and is_tie():
-                Draw_Board()
-                print("\nYou lost!")
-                break               
-            if is_victory("X"):
-                Draw_Board()
-                print("\nYou win Well Done!")
-                break
-            elif is_tie():
-                Draw_Board()
-                print("Its a Tie Game!")
-                break
-            computer_move()
-            if is_victory("O"):
-                Draw_Board()
-                print("\nYou lost!")
-                break
-            elif is_tie():
-                Draw_Board()
-                print("Its a Tie Game!")
-                break
-            elif is_victory('O') and is_tie():
-                Draw_Board()
-                print("\nYou lost!")
-                break  
-    else:
-        while True:
-            computer_move()
-            Draw_Board()
-            if is_victory('O') and is_tie():
-                Draw_Board()
-                print("\nYou lost!")
-                break  
-            elif is_victory("O"):
-                Draw_Board()
-                print("\nYou lost!")
-                break
-            if is_victory("X"):
-                Draw_Board()
-                print("\nYou win Well Done!")
-                break
-            elif is_tie():
-                Draw_Board()
-                print("Its a Tie Game!")
-                break
-            Player_move()
-            if is_victory('X') and is_tie():
-                Draw_Board()
-                print("\nYou win Well Done!")
-                break
-            if is_victory("X"):
-                Draw_Board()
-                print("\nYou win Well Done!")
-                break
-            elif is_victory("O"):
-                Draw_Board()
-                print("\nYou lost!")
-                break
-            elif is_tie():
-                Draw_Board()
-                print("Its a Tie Game!")
-                break
-
- 
-
-def player_move_in_two_player_mode(icon):
+def player_move(icon):
     if icon == "X":
         number = 1
     elif icon == "O":
-        number = 2      
+        number = 2
     print("Your turn player {}".format(number))
     while True:
         choice = input("Enter your move (1-9): ")
         try:
             choice = int(choice)
-            if choice > 0 and choice < 10:
-                choice -= 1
-                if space_is_free(choice):
-                    board[choice] = icon
+            if choice < 10 and choice > 0:
+                if board[choice - 1] == "  ":
+                    board[choice - 1] = icon
                     break
                 else:
-                    print("That space is taken!")
+                    print("\nThat space is taken!")
             else:
-                print('Did you type between [1-9]?')
-        except :
-            print('Did you type a number Please type a number: ')    
+                print('Please type a number between [1-9]!')        
+        except:
+            print('Did you type a number?')
 
+def is_victory(icon):
+    if (board[0] == icon and board[1] == icon and board[2] == icon) or \
+       (board[3] == icon and board[4] == icon and board[5] == icon) or \
+       (board[6] == icon and board[7] == icon and board[8] == icon) or \
+       (board[0] == icon and board[3] == icon and board[6] == icon) or \
+       (board[1] == icon and board[4] == icon and board[7] == icon) or \
+       (board[2] == icon and board[5] == icon and board[8] == icon) or \
+       (board[0] == icon and board[4] == icon and board[8] == icon) or \
+       (board[2] == icon and board[4] == icon and board[6] == icon):
+        return True
+    else:
+        return False
+
+
+def is_draw():
+    if "  " not in board:
+        return True
+    else:
+        return False
 
 def two_player_mode():
-    print('You have picked the Two player mode, First one is: (X) And second one is: (O) -_-')
+    global board
+    board = ["  " for i in range(9)]
     while True:
-        Draw_Board()
-        player_move_in_two_player_mode("X")
-        Draw_Board()
+        print_board()
+        player_move("X")
+        print_board()
         if is_victory("X"):
             print("X Wins! Congratulations!")
             break
-        elif is_tie():
-            print("Its a Tie Game!")
+        elif is_draw():
+            print("Its a draw!")
             break
-        player_move_in_two_player_mode("O")
+        player_move("O")
         if is_victory("O"):
-            Draw_Board()
+            print_board()
             print("O Wins! Congratulations!")
             break
-        elif is_tie():
-            print("Its a Tie Game!")
+        elif is_draw():
+            print("Its a draw!")
+            break
+
+def computer_move():
+    icon = 'O'
+    go = True
+    while go == True:
+        computer_choice = random.randint(0, 8)
+        if board[computer_choice] == "  ":
+            board[computer_choice] = icon
+            go = False
+
+def one_player_mode():
+    global board
+    board = ["  " for i in range(9)]
+    while True:
+        print_board()
+        player_move('X')
+        print_board()
+        if is_victory("X"):
+            print("You Win! Congratulations!")
+            break
+        elif is_draw():
+            print("Its a draw!")
+            break
+        computer_move()
+        if is_victory("O"):
+            print_board()
+            print("You lost!")
+            break
+        elif is_draw():
+            print("Its a draw!")
             break
 
 #==============================
@@ -198,19 +126,24 @@ print('''
     ==========================
     Good Luck!
         '''.format(name))
-
-
-print('Please choose which mode you want to play')
-print('''
-        1.One Player Game
-        2.Two Player Game     
-        ''')
-j = input('Please choose [1-2]: ')
-if j == '1':
-    print('====================')
-    one_player_mode()  
-elif j == '2':
-    print('====================')
-    two_player_mode()
-else:
-    print('OK u did\'nt type 1 or 2 so Goodbye!')
+rerun = 'y'      
+while rerun == 'y':
+    if rerun.lower() == 'y':
+        board = [' ',' ',' ',' ',' ',' ',' ',' ',' '] 
+        print('--------------------')
+        print('Please choose which mode you want to play')
+        print('''
+                1.One Player Game
+                2.Two Player Game     
+                ''')
+        j = input('[1/2]: ')
+        if j == '1':
+            one_player_mode()
+        elif j == '2':
+            two_player_mode()
+        else:
+            print('That means nothing bye!')
+            break    
+    else:
+        break
+    rerun = input("Do you want to play y for yes anything else will end: ")
